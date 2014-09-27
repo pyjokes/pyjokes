37192c7c9d79c6d3454b96d821ed9031d6b0d95b
Ben Nuttall <ben@bennuttall.com>
2014-09-28 01:25:06 +0200

    Implement basic functionality

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-pyjokes
-=======
+# pyjokes
 
 One line jokes for programmers (jokes as a service)
+
+## Usage
+
+```bash
+python3 pyjokes.py
+```
---
+++ pyjokes/pyjokes.py
@@ -0,0 +1,8 @@
+from jokes import jokes
+from random import choice
+
+def main():
+   print(choice(jokes))
+
+if __name__ == '__main__':
+    main()
