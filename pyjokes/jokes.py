94663855f7e29366f74b71e0a938891fcd7f90dc
Borja Ayerdi <ayerdi.borja@gmail.com>
2014-09-28 13:01:54 +0200

    Chuck nerd jokes added.

--- pyjokes/jokes.py
+++
@@ -1,7 +0,0 @@
-jokes = [
-    "A SQL query goes into a bar, walks up to two tables and asks, \"Can I join you?\"",
-    "When your hammer is C++, everything begins to look like a thumb.",
-    "If you put a million monkeys at a million keyboards, one of them will eventually write a Java program. The rest of them will write Perl programs.",
-    "To understand what recursion is, you must first understand recursion.",
-]
-
--- pyjokes/pyjokes.py
+++ pyjokes/pyjokes.py
@@ -1,8 +1,16 @@
-from jokes import jokes
+import json
+import urllib2
 from random import choice
 
+def get_chuck_nerd_jokes():
+	url = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]'
+	response = urllib2.urlopen(url)
+	data = json.load(response)  
+	d = data['value'] 
+	return d['joke']
+
 def main():
-   print(choice(jokes))
+	print get_chuck_nerd_jokes()
 
 if __name__ == '__main__':
     main()
