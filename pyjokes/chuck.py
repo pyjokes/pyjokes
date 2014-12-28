31752dc1d387000b04aa49d5bc0b89c5f4dbf348
Ben Nuttall <ben@bennuttall.com>
2014-12-28 23:29:11 +0000

    Make function name singular, fix read/readall call

--- pyjokes/chuck.py
+++ pyjokes/chuck.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import json
 
 try:
@@ -8,13 +6,13 @@ except:
     from urllib.request import urlopen
 
 
-def get_chuck_nerd_jokes():
+def get_chuck_nerd_joke():
     url = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]'
-    response = urlopen(url).readall().decode('utf-8')
+    response = urlopen(url).read().decode('utf-8')
     data = json.loads(response)
     d = data['value']
     return d['joke']
 
 
 if __name__ == '__main__':
-    print(get_chuck_nerd_jokes())
+    print(get_chuck_nerd_joke())
