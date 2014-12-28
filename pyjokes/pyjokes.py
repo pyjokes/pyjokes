2c7b03079a082b34a96ffd6d46b9723377dd2480
Ben Nuttall <ben@bennuttall.com>
2014-12-28 23:28:17 +0000

    Tidy up

--- pyjokes/pyjokes.py
+++ pyjokes/pyjokes.py
@@ -1,9 +1,11 @@
-from random import choice
+from __future__ import absolute_import
+import random
 from .jokes import jokes
 
 
 def get_local_joke():
-    return choice(jokes)
+    return random.choice(jokes)
+
 
 if __name__ == '__main__':
     print(get_local_joke())
