3881770ebf936ee21839731447ff2c445a8a74af
Alexandre M. S <alexsavio@gmail.com>
2014-12-31 13:58:50 +0100

    Make get_local_joke read from the new joke lists

--- pyjokes/pyjokes.py
+++ pyjokes/pyjokes.py
@@ -1,9 +1,23 @@
 from __future__ import absolute_import
 import random
-from .jokes import jokes
+from .geek  import geek_neutral, geek_explicit
+from .chuck import chuck_neutral, chuck_explicit, chuck_nerdy
 
 
-def get_local_joke():
+def join_joke_sets(joke_sets):
+    jokes_set = set(joke_sets)
+    jokes     = []
+    try:
+        for set_name in jokes_set:
+            jokes.extend(eval(set_name))
+        return jokes
+    except:
+        print('Could not find joke set {}.'.format(set_name))
+        raise
+
+
+def get_local_joke(joke_sets=['geek_neutral']):
+    jokes = join_joke_sets(joke_sets)
     return random.choice(jokes)
 
 
