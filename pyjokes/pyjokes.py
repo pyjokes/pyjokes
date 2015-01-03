a997301c907005e7bf9db0d76b47d6d11bda0168
Ben Nuttall <ben@bennuttall.com>
2015-01-03 12:29:03 +0000

    Import new jokes lists, allow selection

--- pyjokes/pyjokes.py
+++ pyjokes/pyjokes.py
@@ -1,11 +1,16 @@
 from __future__ import absolute_import
 import random
-from .jokes import jokes
+from .jokes import geek_neutral, geek_explicit, chuck_nerdy
 
-
-def get_local_joke():
+def get_joke(category='neutral'):
+    jokes = {
+        'neutral': geek_neutral,
+        'explicit': geek_explicit,
+        'chuck': chuck_nerdy,
+        'all': geek_neutral + geek_explicit + chuck_nerdy,
+    }[category]
     return random.choice(jokes)
 
 
 if __name__ == '__main__':
-    print(get_local_joke())
+    print(get_joke('all'))
