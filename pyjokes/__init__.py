23feb987dda20732d2ca837c2fb8269f4ce7cda9
Ben Nuttall <ben@bennuttall.com>
2015-01-03 12:46:05 +0000

    Add jokes list to __init__.py

--- pyjokes/__init__.py
+++ pyjokes/__init__.py
@@ -2,5 +2,7 @@ from __future__ import absolute_import
 from .pyjokes import get_joke
 from .jokes import geek_neutral, geek_explicit, chuck_nerdy
 
+jokes = geek_neutral + geek_explicit + chuck_nerdy
+
 
 __version__ = '0.1.2'
