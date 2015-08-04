652225d376355be8bba2c3378d0bf8be92ba7058
Oier Echaniz <oier.etxaniz@gmail.com>
2015-08-04 10:10:22 +0200

    setup with pytest

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 import os
 from setuptools import setup, find_packages
-
+from setuptools.command.test import test as TestCommand
+import sys
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
     name="pyjokes",
@@ -27,4 +38,7 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
+    #test requirements and class specification:
+    test_require=['pytest'],
+    cmdclass={ 'test' : PyTest },
 )
