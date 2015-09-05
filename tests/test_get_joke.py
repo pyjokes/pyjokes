91a7e4ba30c2c455c58b7069015680b7af511cc4
Alexandre M. S <alexsavio@gmail.com>
2015-09-05 22:31:33 +0200

    Simplify get_joke test, add raise checks

--- tests/test_get_joke.py
+++ tests/test_get_joke.py
@@ -1,15 +1,25 @@
 
+import pytest
+from   pyjokes import get_joke
+from   pyjokes.pyjokes import LanguageNotFoundError, CategoryNotFoundError
+
 
 def test_get_joke():
-    from pyjokes import get_joke
+    assert get_joke()
 
-    for i in range(10):
-        assert get_joke()
-
-    languages = ['eng', 'de', 'spa']
+    languages = ['en', 'de', 'es']
     categories = ['neutral', 'explicit', 'all']
 
     for lang in languages:
-        for cat in categories:
-            for i in range(10):
-                assert get_joke(cat, lang)
+        assert get_joke(language=lang)
+
+    for cat in categories:
+        assert get_joke(category=cat)
+
+
+def test_get_joke_raises():
+    assert pytest.raises(LanguageNotFoundError, get_joke, language='eu')
+
+    assert pytest.raises(LanguageNotFoundError, get_joke, language='tr')
+
+    assert pytest.raises(CategoryNotFoundError, get_joke, category='123')
