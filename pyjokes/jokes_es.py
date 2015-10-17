eaa7808cb07213ed7ae8976b3830f99ceface7ca
ResonantWave <nixijav@nixijav.com>
2015-10-17 12:43:32 +0200

    added more jokes in spanish

--- pyjokes/jokes_es.py
+++ pyjokes/jokes_es.py
@@ -5,6 +5,21 @@ neutral = [
     'Se abre el ascensor y hay un programador dentro, le preguntan: - Sube o baja? A lo que el programador responde: - Si.',
     #'Qué le dice un bit al otro? Nos vemos en el bus.',
     'Que le dice un bit al otro? Nos vemos en el bus.',
+<<<<<<< Updated upstream
+=======
+    'Atracador: ¡El dinero o la vida! Programador: Lo siento, soy programador. Atracador: ¿Y? Programador: No tengo dinero ni vida.',
+    '¿Qué es un terapeuta? - 1024 Gigapeutas',
+    '¿Cuántos programadores hacen falta para cambiar una bombilla? - Ninguno, porque es un problema de hardware.',
+    'Se abre el telón, aparece un informático y dice: ¡qué habéis tocado que no se cierra el telón!',
+    '- ...fallece una persona mientras estudiaba en la biblioteca - ¿Que estaba estudiando? - La API de Windows.',
+    '¿Qué le dice un GIF a un JPG? -Anímate hombre...',
+    '¿Qué dice un informático que se está ahogando en la playa?: ¡F1, F1!',
+    'Sólo hay 10 tipos de personas en el mundo, las que entienden binario y las que no.',
+    'Sólo hay 10 tipos de personas en el mundo: las que entienden trinario, las que no, y las que lo confunden con binario.',
+    'Una mujer dice a su marido informático: - Ve al supermercado y trae una barra de pan, y si hay huevos, doce. Volvió con doce barras de pan.',
+    '¿Quién demonios es el general Failure, y qué hace intentando leer de mi disco duro?',
+    '¿En qué se diferencian Windows y un virus? En que el virus funciona, y es gratis.'
+>>>>>>> Stashed changes
 ]
 
 adult = [
