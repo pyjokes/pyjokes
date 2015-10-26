ea18791d20c1f87e046bb0119c5eb43c6441143d
Jay Fault <alchzh@gmail.com>
2015-10-26 16:37:29 -0400

    Add a joke from the stack overflow
    from (http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke?page=2&tab=votes#tab-top)[here]
    funny?

--- pyjokes/jokes_en.py
+++ pyjokes/jokes_en.py
@@ -94,7 +94,8 @@ neutral = [
     "Microsoft hold a bi-monthly internal \"productive week\" where they use Google instead of Bing.",
     "Schrodinger's Cat in Web Development: If I don't look at in in Internet Explorer then there's a chance it looks fine.",
     "Finding a good PHP developer is like looking for a needle in a haystack. Or is it a hackstack in a needle?",
-    "A COBOL programmer makes millions with Y2K remediation and decides to get cryogenically frozen. \"It's 9999BC. You know COBOL right?\""
+    "A COBOL programmer makes millions with Y2K remediation and decides to get cryogenically frozen. \"It's 9999BC. You know COBOL right?\"",
+    "The C language combines all the power of assembly language with all the ease-of-use of assembly language."
 ]
 
 adult = [
