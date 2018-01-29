9ba8fdf8071c7aaf171cc4d60d4ae0cd3afc7781
Ale <aleale99@yahoo.com>
2018-01-29 19:47:50 +0100

    insert italian

--- tests/test_joke_length.py
+++ tests/test_joke_length.py
@@ -3,6 +3,7 @@ from pyjokes.jokes_de import jokes_de
 from pyjokes.jokes_es import jokes_es
 from pyjokes.jokes_gl import jokes_gl
 from pyjokes.jokes_eu import jokes_eu
+from pyjokes.jokes_it import jokes_it
 
 
 def _test_joke_length(joke):
@@ -15,6 +16,6 @@ def _test_joke_group(jokes):
 
 
 def test_jokes_lengths():
-    jokes_sets = [jokes_en, jokes_es, jokes_de, jokes_gl, jokes_eu]
+    jokes_sets = [jokes_en, jokes_es, jokes_de, jokes_gl, jokes_eu, jokes_it]
     for jokes in jokes_sets:
         _test_joke_group(jokes['all'])
