6392fe83bc39b651a358aafb0e04a1b88f166771
Roro Okpu <roro.okpu@ask.com>
2018-06-10 11:54:37 -0700

    Added some more neutral jokes

--- pyjokes/jokes_en.py
+++ pyjokes/jokes_en.py
@@ -101,6 +101,21 @@ neutral = [
     "What does 'Emacs' stand for? 'Exclusively used by middle aged computer scientists.'",
     "What does PyJokes's jokes have in common with Adobe Flash? They get updated all the time, but they don't get any better.",
     "How many demosceners are needed to change a lightbulb? Half. With a full one there is no challenge.",
+    "Did you hear the one about the guy with the broken hearing aid? Neither did he.",
+    "What do you call a fly without wings? A walk.",
+    "When my wife told me to stop impersonating a flamingo, I had to put my foot down.",
+    "What do you call someone with no nose? Nobody knows.",
+    "What time did the man go to the dentist? Tooth hurt-y.",
+    "How many optometrists does it take to change a light bulb? 1 or 2? 1... or 2?",
+    "I was thinking about moving to Moscow but there is no point Russian into things.",
+    "Why does Waldo only wear stripes? Because he doesn't want to be spotted.",
+    "Do you know where you can get chicken broth in bulk? The stock market.",
+    "I used to work for a soft drink can crusher. It was soda pressing.",
+    "I went to the zoo the other day, there was only one dog in it. It was a shitzu.",
+    "I gave all my dead batteries away today, free of charge.",
+    "Why are skeletons so calm? Because nothing gets under their skin.",
+    "What did celery say when he broke up with his girlfriend? She wasn't right for me, so I really don't carrot all.",
+    "What's the worst thing about ancient history class? The teachers tend to Babylon."
 ]
 
 
