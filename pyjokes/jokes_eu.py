0c89a91467821f327dfdb2b00ddd8bc9c64df071
Nina Zakharenko <nzakharenko@gmail.com>
2018-06-28 23:10:59 -0700

    Remove questionable jokes in other languages

--- pyjokes/jokes_eu.py
+++ pyjokes/jokes_eu.py
@@ -5,7 +5,6 @@ neutral = [
     'Zer esaten dio bit batek besteari? Busean ikusten gara!',
     'Zer da terapeuta bat? - 1024 Gigapeuta',
     'Aita, aita, aita!!! Utziko al didazu telebista iksten? Bai! Baina piztu ez!',
-    'Matematika liburu batek bere buruaz beste egin zuen...zergatik? Problema asko zituelako.'
 ]
 
 jokes_eu = {
