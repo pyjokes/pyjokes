b0282aa948fd20577512eb9aaea4f30319157f67
Marek Bukal <mara@loom.dvl>
2020-05-04 15:51:47 +0200

    initial support cs-cz language

---
+++ pyjokes/jokes_cs.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+neutral = [
+    "Blondýnka jede instruktorem autoškoly. Ten náhle volá: Objet! Ale slečna říká: Ale já ještě nemám hlad.",
+    "Víte, proč blondýna pokládá vedle počítače sýr? Aby měla myš co jíst!",
+    "Ptá se blondýna blondýny: Už si četla Shakespeara? Blondýna se zamyslí a řekne: A kdo ho napsal?",
+    "Blondýnka na letišti: Slyšela jsem, že letí červený lodičky, tak jsem se přišla zeptat, v kolik?",
+    "Jedna blondýna se ptá druhé: Kde ses narodila? V nemocnici. Fakt? A co ti bylo?",
+    "Hrají dvě blondýny šachy a jedna říká GOOOL!!! A ta druhá na to „To není tenis ty blbá!",
+    "Vynálezce automatických oprav textu zemřel. Jeho potřeb se bude kopat v úvěry večer.",
+    "Za manžela jsem si vzala programátora. Když jsem se ho zeptala, jak mám otevřít jar, řekl mi, abych si stáhla Javu.",
+    "Před chvílí jsem zažil nejrychlejší přenos dat v životě, 256 GB za sekundu. Vysavač mi vcucl flešku.",
+    "Vnouček křičí: Babi, jdeme koupit playstation! Babička odpoví: Kupte si co chcete, já to krmit nebudu!",
+    "Tak cos dneska dělal ve škole, Pepíčku? Já o tom nechci mluvit, mami. Večer to stejně uvidíš ve zprávách.",
+    "Proč mi lžeš, Pepíčku? ptá se učitelka. Víš, co se stane z dětí, které lžou? Pepíček odpoví: Politici nebo poslanci, paní učitelko.",
+    "V hodině zpěvu vyvolá paní učitelka Pepíčka, aby vyjmenoval bicí nástroje. Pepíček odpoví: Tak to je rákoska, vařečka a řemen!",
+    "Doktoři říkají, že se v noci nemá jíst. No jo, ale proč tedy v ledničce je to světlo?",
+
+]
+
+chuck = [
+    "Chuck Norris jednou v Atlantidě prohrál člověče nezlob se. Atlantida je doteď pod vodou.",
+    "Víte co proletí hlavou každého nepřítele Chucka Norrise? Kop z otočky do držky.",
+    "Chuck Norris nepotřebuje klávesnici a myš. Počítač ho poslouchá na slovo",
+    "Kolik Chucků Norrisů je potřeba k výměně žárovky? Špatná otázka! Správně je: Kolik žárovek je potřeba k výměně Chucka Norrise?",
+    "Chuck Norris dal hattrick. Z trestné lavice!",
+    "Chuck Norris se dokáže na jedno nadechnutí potopit na dno Mariánského příkopu!",
+    "Bermudský trojúhelník byl původně Bermudský čtverec, to bylo předtím, než mu Chuck Norris ukopl jeden roh!",
+    "Binární kód počítače obsahuje jedničky a nuly. Binární kód Chuck Norrisova mozku obsahuje pouze jedničky.",
+    "Proč Chuck Norris nechodí do kina? Protože ho už viděl.",
+    "První zákon termodynamiky říká, že energie nemůže být ani vytvořena, ani zničena. Pokud nepotká Chucka Norrise.",
+    "Prvních několik milionů svých protivníků Chuck Norris pomlátil už jako spermie.",
+    "Chuck Norris už je vlastně mrtvý - jen se mu to Smrtka ještě neodvážila říct.",
+    "Chuck Norris dokáže prásknout lítačkama.",
+    "Chuck Norris poprvé vyfotil Slunce v noci.",
+    "Tvrdé souhlásky: H, CH, K, R, D, T, N, Chuck Norris.",
+    "Víte, jaký je rozdíl mezi Vladimírem Putinem a Chuckem Norrisem? Chuck Norris si nemyslí, že je Vladimír Putin.",
+    "Chuck Norris je rychlejší než stín.",
+    "Chuck Norris může mít botu v kameni.",
+    "Jen Chuck Norris dokáže mít vzpomínky na budoucnost.",
+]
+
+jokes_cs = {
+    'neutral': neutral,
+    'chuck': chuck,
+    'all': neutral + chuck,
+}
