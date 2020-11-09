14a015d7de2629ec728fc1301163f699823a9ee3
Alfred Dagenais <jesuis@alfreddagenais.com>
2020-11-08 19:44:28 -0500

    ADD: some french jokes

--- pyjokes/jokes_fr.py
+++ pyjokes/jokes_fr.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-
 
 neutral = [
-    'No jokes added yet.',
+    "Optimiste: Le verre est à moitié plein. Pessimiste: Le verre est à moitié vide. Programmeur: Le verre est 2 fois plus grand que nécessaire.",
+    "Où les programmeurs sortent habituellement. R. Foo Bar",
+    "Comment comparer HTML et HTML5? Est-ce que cela fonctionne avec Internet Explorer? Non? C'est du HTML5.",
+    "Les vrais programmeurs comptent à partir de 0",
+    "C'est l'histoire d'un programmeur qui entre dans un bar et qui dit 'Hello World'",
+    "3 bases de données SQL rentrent dans un bar NoSQL. Quelque temps plus tard, ils en sortent, car ils n'ont pas trouvé de table.",
+    "Une requête SQL entre dans un bar, se met à deux tables et demande : 'Je peux me joindre à vous?'",
+    "S'il est écrit: Ce logiciel requière Windows XP ou mieux. Est-ce que cela veut dire qu'il peut rouler sous Linux?",
+    "Je voudrais faire un meilleur monde, mais on ne veut pas me donner le code source.",
+    "Ne jamais copier et coller toute une fonction. Toujours créer une nouvelle méthode.",
+    "C'est C++ qui dit à C: 'Voyons! Tu n'as pas de classe'",
+    "C'est C++ qui dit à C: 'Pourquoi, ne viens-tu jamais en classe?'",
+    "Il y a 2 types de programmeurs. 1) Je suis Dieu. 2) Je n’ai aucune idée de ce que je fais.",
+    "Il y a 10 types de personnes: ceux qui savent compter en binaire, et les autres.",
+    "Si Microsoft inventait un truc qui ne plante pas, ce serait un clou",
+    "Combien faut-il de programmeurs pour changer une ampoule? Aucun c'est un problème matériel.",
+    "Quelle est la différence entre Windows XP et un virus? Le virus fonctionne sans aide.",
+    "Quelle est la différence entre Bill Gates et Dieu ? Dieu ne se prend pas pour Bill Gates.",
+    "Un programmeur dit à un autre programmeur. Je ne comprends pas, cela fonctionnait mercredi dernier.",
+    "Un programmeur expérimenté se pose toujours des questions lorsque le code compile du premier coup.",
+    "Toc Toc. Qui est là ? *Très longue pause* C'est Internet Explorer.",
+    "C'est l'histoire d'un administrateur qui configure ses variables d'environnement, et là... PATH le chemin !",
+    "Un professeur dit à ses élèves que pour réussir, ils ne doivent pas se fier à un moteur de recherche... Le programmeur rit très fort!",
+    "Un programmeur se dira toujours: Cela ne fonctionne pas, pourquoi? Et, cela fonctionne, pourquoi?",
 ]
 
 jokes_fr = {
