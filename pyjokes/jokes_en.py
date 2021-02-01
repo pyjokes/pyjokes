1c789ecb7982db199a4552eb43206e6ff9984933
Rashh <70662641+rash-c9@users.noreply.github.com>
2021-02-01 11:56:39 +0530

    added a joke to jokes_en.py

--- pyjokes/jokes_en.py
+++ pyjokes/jokes_en.py
@@ -6,6 +6,7 @@ http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke?page
 """
 
 neutral = [
+    "Once there was a programmer. He was frustrated, no doubt... still he is"
     "Complaining about the lack of smoking shelters, the nicotine addicted Python programmers said there ought to be 'spaces for tabs'.",
     "Ubuntu users are apt to get this joke.",
     "Obfuscated Reality Mappers (ORMs) can be useful database tools.",
