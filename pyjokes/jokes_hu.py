40224e1983b568e6f760ef1d6acac1c872044f7c
Székely Márton <53979153+PiciAkk@users.noreply.github.com>
2021-11-20 18:37:50 +0100

    Update jokes_hu.py

--- pyjokes/jokes_hu.py
+++ pyjokes/jokes_hu.py
@@ -13,8 +13,8 @@ neutral = [
     "10-féle ember létezik. Az egyik ismeri a bináris számrendszert, a másik nem.",
     "- Hogyan próbálnak meggazdagodni az objektumorientált programozók? - ??? - Öröklődés által.",
     "- Mi a teendő tűz esetén? - ??? - 1. `git add .` 2. `git commit` 3. `git push` 4. hagyjuk el az épületet",
-    "- Mit csinál a backend fejlesztő, amikor sikeresen megváltoztat valamit CSS-ben? - ??? - Beírja az önéletrajzába, hogy full stack fejlesztő"
-    "- Hogy hívsz nyolc hobbitot? - ??? - Egy hobbyte.",
+    "- Mit csinál a backend fejlesztő, amikor sikeresen megváltoztat valamit CSS-ben? - ??? - Beírja az önéletrajzába, hogy full stack fejlesztő",
+    "- Hogy hívsz nyolc hobbitot? - ??? - Egy hobbyte."
 ]
 
 chuck = [
