feddbf5148f385be6347f38aa48e50db5590efd0
Petint <petras.balint04@gmail.com>
2022-06-13 22:16:42 +0200

    Removed jokes that fail the encoding tests

--- pyjokes/jokes_hu.py
+++ pyjokes/jokes_hu.py
@@ -15,17 +15,8 @@ neutral = [
     "Két programozó beszélget: - Milyen a kedved? - Egész változó... Integer?",
     "Mire táncolnak a programozók a buliban? Algoritmusra.",
     "10-féle ember létezik. Az egyik ismeri a bináris számrendszert, a másik nem.",
-    "Hogyan próbálnak meggazdagodni az objektumorientált programozók? Öröklődés által.",
-    "Mi a teendő tűz esetén? 1. `git add .` 2. `git commit` 3. `git push` 4. hagyjuk el az épületet",
-    "Mit csinál a backend fejlesztő, amikor sikeresen megváltoztat valamit CSS-ben? Beírja az önéletrajzába, hogy full stack fejlesztő",
     "Hogy hívsz nyolc hobbitot? Egy hobbyte.",
-    "Mit mond egy GitHub felhasználő amikor megtudja hogy ellopták a kódját? 'Köszönöm a fork-ot.'",
-    "A programozó elmegy a hamburgereshez. Megkérdezik tőle: -Ketchup vagy majonéz? Programozó: -Igen",
     "A programozó vizsgázik. -A 0 az hamis, az 1 pedig igaz, nemde? -1.",
-    "Miért mondható el, hogy a programozók mind őrültek? Ha éppen nincs semmi problémájuk, akkor kitalálnak egyet saját maguk számára.",
-    "Egy COBOL programozó milliókat keres az Y2K kármentesítéssel, és úgy dönt, hogy kriogén módon lefagyasztja magát. \"Az év 9999. Ismeri a COBOL-t, ugye?\"",
-    "A C nyelv az assembly nyelv minden erejét egyesíti az assembly nyelv könnyű használhatóságával.",
-    "Egy SEO szakértő besétál egy bárba, bárba, kocsmába, nyilvános házba, ír kocsmába, kocsmába, csaposba, sörbe, italba, borba, alkoholba, szeszes italokba...",
     "Mit jelent az \"Emacs\"? \"Kizárólag középkorú informatikusok használják.\"",
     "Mi a közös a pyjokes és az Adobe Flash között? Folyamatosan frissül, de soha nem lesz jobb."
 ]
@@ -33,26 +24,16 @@ neutral = [
 chuck = [
     "Miért Chuck Norris a világ legjobb programozója? Mert olyan kódot ír, ami saját magát képes optimalizálni.",
     "Amikor Chuck Norris kivételeket dob, az a szoba túloldalán van.",
-    "Minden tömb, amit Chuck Norris deklarál, végtelen méretű, mert Chuck Norris nem ismer határokat.",
     "Chuck Norrisnak nincs lemezkésleltetése, mert a merevlemez tudja, hogy sietnie kell a pokolba, különben.",
     "Chuck Norris olyan kódot ír, amely optimalizálja önmagát.",
-    "Chuck Norris nem tesztelheti az egyenlőséget, mert nincs párja.",
     "Chuck Norrisnak nincs szüksége szemétszállításra, mert nem a .Dispose()-t hívja, hanem a .DropKick().-t",
-    "Chuck Norris első programja a kill -9 volt.",
     "Chuck Norris szétrobbantotta a dot com buborékot.",
-    "Chuck Norris egy lépésből meg tudja oldani a hanoi tornyokat.",
     "Az egyetlen tervezési minta, amelyet Chuck Norris ismer, az az Isten objektum mintája.",
     "Chuck Norris befejezte a World of Warcraftot.",
     "A projektmenedzserek soha nem kérnek Chuck Norristól becsléseket.",
     "Chuck Norris nem használ webes szabványokat, mivel a web megfelelni fog neki.",
-    "\"Az én gépemen működik\" Chuck Norrisra mindig igaz.",
     "Chuck Norris nem Burn Down listákat, hanem Smack Down slágereket készít.",
-    "Chuck Norris szakálla percenként 140 szót tud gépelni.",
-    "Chuck Norris képes teljes alkalmazásokat tesztelni egyetlen kijelentéssel: \"működik\".",
-    "Chuck Norris nem bugokra vadászik, mivel ez a kudarc valószínűségét jelenti, hanem bugokat gyilkol.",
-    "Chuck Norris billentyűzetén nincs Ctrl billentyű, mert semmi sem irányítja Chuck Norrist.",
-    "Chuck Norris túlcsordulhat a veremből, ha csak ránéz.",
-    "Chuck Norris számára minden tartalmaz egy sebezhetőséget."
+    "Chuck Norris szakálla percenként 140 szavat tud gépelni.",
 ]
 
 jokes_hu = {
