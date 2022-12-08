d35c83693884c62a1fdb04b93c84411838170e85
Pedro Rey Anca <personal@peprolinbot.com>
2022-12-08 12:22:32 +0000

    Fix typos
    Fix typos. Mostly non-opened exclamations and interrogations

--- pyjokes/jokes_es.py
+++ pyjokes/jokes_es.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 
 neutral = [
-    'Se abre el ascensor y hay un programador dentro, le preguntan: - Sube o baja? A lo que el programador responde: - Si.',
-    'Que le dice un bit al otro? Nos vemos en el bus.',
-    'Atracador: El dinero o la vida! Programador: Lo siento, soy programador. Atracador: Y? Programador: No tengo dinero ni vida.',
-    'Que es un terapeuta? - 1024 Gigapeutas',
-    'Cuantos programadores hacen falta para cambiar una bombilla? - Ninguno, porque es un problema de hardware.',
-    'Se abre el telon, aparece un informatico y dice: que habeis tocado que no se cierra el telon!',
-    '- ...fallece una persona mientras estudiaba en la biblioteca - Que estaba estudiando? - La API de Windows.',
-    'Que le dice un GIF a un JPG? -Animate hombre...',
-    'Que dice un informatico que se esta ahogando en la playa?: F1, F1!',
-    'Solo hay 10 tipos de personas en el mundo, las que entienden binario y las que no.',
+    'Se abre el ascensor y hay un programador dentro, le preguntan: ¿Sube o baja? A lo que el programador responde: Sí.',
+    '¿Qué le dice un bit a otro? Nos vemos en el bus.',
+    'Atracador: ¡El dinero o la vida! Programador: Lo siento, soy programador. Atracador: ¿Y? Programador: No tengo ni dinero ni vida.',
+    '¿Qué es un terapeuta? 1024 Gigapeutas.',
+    '¿Cuántos programadores hacen falta para cambiar una bombilla? Ninguno, porque es un problema de hardware.',
+    'Se abre el telón, aparece un informático y dice: ¡Qué habéis tocado que no se cierra el telón!',
+    'Fallece una persona mientras estudiaba en la biblioteca. ¿Qué estaba estudiando? La API de Windows.',
+    '¿Qué le dice un GIF a un JPG? Anímate hombre...',
+    '¿Qué dice un informático que se está ahogando en la playa?: ¡F1, F1!',
+    'Solo hay 10 tipos de personas en el mundo: las que entienden binario y las que no.',
     'Solo hay 10 tipos de personas en el mundo: las que entienden trinario, las que no, y las que lo confunden con binario.',
-    'Una mujer dice a su marido informatico: - Ve al supermercado y trae una barra de pan, y si hay huevos, doce. Trajo doce barras de pan.',
-    'Quien demonios es el general Failure, y que hace intentando leer de mi disco duro?',
-    'En que se diferencian Windows y un virus? En que el virus funciona, y es gratis.'
+    'Una mujer le dice a su marido informático: Ve al supermercado y trae una barra de pan, y si hay huevos, doce. Trajo doce barras de pan.',
+    '¿Quién demonios es el General Failure, y qué hace intentando leer mi disco duro?',
+    '¿En qué se diferencian Windows y un virus? En que el virus funciona, y es gratis.'
 ]
 
 jokes_es = {
