8d5a4b3eebc92cb23c6efc57a68184ccbd0dea8f
rapolasLT <36411768+rapolasLT@users.noreply.github.com>
2023-12-21 14:12:22 +0200

    Added new language (#94)
    * Added new language
    
    Added Lithuanian language to the main file
    
    * Update pyjokes.py
    
    ---------
    
    Co-authored-by: alexsavio <alexsavio@users.noreply.github.com>

--- pyjokes/pyjokes.py
+++ pyjokes/pyjokes.py
@@ -8,6 +8,7 @@ from .jokes_fr import jokes_fr
 from .jokes_gl import jokes_gl
 from .jokes_eu import jokes_eu
 from .jokes_it import jokes_it
+from .jokes_lt import jokes_lt
 from .jokes_hu import jokes_hu
 
 all_jokes = {
@@ -18,6 +19,7 @@ all_jokes = {
     'gl': jokes_gl,
     'eu': jokes_eu,
     'it': jokes_it,
+    'lt': jokes_lt,
     'hu': jokes_hu
 }
 
@@ -37,7 +39,7 @@ def get_jokes(language='en', category='neutral'):
     category: str
         Choices: 'neutral', 'chuck', 'all', 'twister'
     lang: str
-        Choices: 'en', 'de', 'es', 'fr', 'gl', 'eu', 'it', 'hu'
+        Choices: 'en', 'de', 'es', 'fr', 'gl', 'eu', 'it', 'hu', 'lt'
 
     Returns
     -------
@@ -62,7 +64,7 @@ def get_joke(language='en', category='neutral'):
     category: str
         Choices: 'neutral', 'chuck', 'all', 'twister'
     lang: str
-        Choices: 'en', 'de', 'es', 'fr', 'gl', 'eu', 'it', 'hu'
+        Choices: 'en', 'de', 'es', 'fr', 'gl', 'eu', 'it', 'hu', 'lt'
 
     Returns
     -------
