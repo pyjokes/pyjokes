5289956281cb363851dd69cf84d931a8628c8c5c
simsononroad <d-github@kopaszhegy.hu>
2025-05-08 17:50:52 +0200

    add more hungarian jokes

--- pyjokes/jokes_hu.py
+++ pyjokes/jokes_hu.py
@@ -3,9 +3,9 @@
 # HUNGARIAN
 
 """
-Most of the jokes are from https://viccfaktor.hu/cimke/programozo-viccek/, and https://gremmedia.hu/programozo-viccek
+Most of the jokes are from https://viccfaktor.hu/cimke/programozo-viccek/, and https://gremmedia.hu/programozo-viccek and https://borsosfi.hu/humor/informatikus-viccek
 //
-A viccek nagy része a https://viccfaktor.hu/cimke/programozo-viccek/ weboldalról és a https://gremmedia.hu/programozo-viccek weboldalról származik
+A viccek nagy része a https://viccfaktor.hu/cimke/programozo-viccek/ weboldalról és a https://gremmedia.hu/programozo-viccek és a https://borsosfi.hu/humor/informatikus-viccek weboldalról származik
 """
 
 neutral = [
@@ -17,6 +17,13 @@ neutral = [
     "- Mi a teendő tűz esetén? - ??? - 1. `git add .` 2. `git commit` 3. `git push` 4. hagyjuk el az épületet",
     "- Mit csinál a backend fejlesztő, amikor sikeresen megváltoztat valamit CSS-ben? - ??? - Beírja az önéletrajzába, hogy full stack fejlesztő",
     "- Hogy hívsz nyolc hobbitot? - ??? - Egy hobbyte.",
+    "A programozó vizsgázik.\n\t-A 0 az hamis, az 1 pedig igaz, nemde?\n\t-1.",
+    "Miért mondható el, hogy a programozók mind őrültek?\n\t-Ha éppen nincs semmi problémájuk, akkor kitalálnak egyet saját maguk számára.",
+    "- Hogy mondják a számítástechnikában, hogy ha egy étteremben a fatányérost porcelán tányérban tálalják fel?\n\t- Fatal error.",
+    "- Hogy hívják az orosz számítógép hálózatot?\n\t- Nyetwork.",
+    "- Hogy hívják a templomok közti hálózatot?\n\t- Paplan.",
+    "- Hol terem a CD ROM?\n\t- Diszkréten.",
+    "- Mit csinál az internetező rab a börtönben?\n\t- Letölti a büntetését!"
 ]
 
 chuck = [
